6d5bbf2d090e77a8c0a212f2887614e6d9d98c33
Matthew Mesaros <mesaros.creative@gmail.com>
2019-02-14 22:39:25 -0600

    Revert "Update front-page.php"
    This reverts commit 71c4ebe50ba4ad834febc2bf5f6b0630b77c8795.

--- wp-content/themes/Green Gravel 2017/front-page.php
+++ wp-content/themes/Green Gravel 2017/front-page.php
@@ -1,18 +1,18 @@
 <?php get_header(); ?>
 
-<?php get_template_part( 'module', 'tickets' ); ?>
+<?php /* get_template_part( 'module', 'tickets' ); ?>
 
 <?php get_template_part( 'module', 'news' ); ?>
 
-<?php get_template_part( 'module', 'slider' );  ?>
+<?php /* get_template_part( 'module', 'slider' ); */ ?>
 
-<article class="padded" style="text-align: center;">
+<!--<article class="padded" style="text-align: center;">
 
 <header><h3 class="flag">2017 Schedule</h3></header>
 
 <a href="<?php bloginfo('template_directory'); ?>/img/program-2017-web-large.pdf" style="clear:both;"><img src="<?php bloginfo('template_directory'); ?>/img/program-2017-web.jpg" /></a>
 
-</article>
+</article>-->
 
 <article>
   
